eaa0a16081c25e31a7467ad1d1ab554fb2ed7991
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-20 16:44:58 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(startPoint){
+    if (startPoint < 42) {
+    return 42 - startPoint
+    } else {
+    return startPoint - 42
+    }
+}
+
+function distanceFromHqInFeet(startPoint) {
+    return distanceFromHqInBlocks(startPoint) * 264
+}
+
+function distanceTravelledInFeet(start,destination) {
+    if (start < destination) {
+        return ((destination - start) * 264)
+    } else if (start > destination) {
+        return ((start - destination) * 264)
+        }
+}
+
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start,destination) <= 400) {
+        return 0
+
+    } else if (distanceTravelledInFeet(start,destination)<= 2000) {
+        return ((distanceTravelledInFeet(start,destination)-400)*.02)
+    
+    } else if (distanceTravelledInFeet(start,destination) < 2500) {
+        return 25
+
+    } else {
+        return 'cannot travel that far'
+    }
+}
